0ae3bd6614e7595d5bf2164bd112da0f2fcbe475
jergus <loldift@gmail.com>
2026-03-10 14:46:54 +0100

    riesenie zleho zobrazenia nav bar menu na mobile13

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -572,7 +572,7 @@
                 @endauth
 
                 @guest
-                    <a class="btn btn-outline-light btn-sm me-2" href="{{ route('login') }}">Prihlásiť sa</a>
+                    <a class="nav-link me-2" href="{{ route('login') }}">Prihlásiť sa</a>
                     <a class="btn btn-sm" style="background-color: var(--brand-orange); color:#fff; border-radius: 9999px; padding-inline: 1rem;" href="{{ route('register') }}">Registrácia</a>
                 @endguest
             </div>
